b95270e40b1d4f4583f0e8b3cf088d6e3892ae68
ToucanBran <brandon.gomez89@gmail.com>
2017-01-08 12:10:52 -0800

    Added in test resources and test class

--- SecretSantaGenerator/src/test/java/com/data/DataValidatorTest.java
+++ SecretSantaGenerator/src/test/java/com/data/DataValidatorTest.java
@@ -9,12 +9,16 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -24,13 +28,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.generator.SecretSanta;
+import com.gui.Constants;
+import com.gui.SecretSantaDisplayType;
+import com.opencsv.CSVReader;
 
 public class DataValidatorTest
 {
     private static final String TEST_DATA2_PASS_FILE_PATH = "/validator_test_data1_pass.csv";
+    private static final String TEST_DATA3_PASS_FILE_PATH = "/validator_test_data2_pass.csv";
     private static final String TEST_EXCLUSIONS2_PASS_FILE_PATH = "/validator_test_exclusions1_pass.csv";
     private static final String TEST_DATA2_FAIL_FILE_PATH = "/validator_test_data1_fail.csv";
     private static final String TEST_EXCLUSIONS2_FAIL_FILE_PATH = "/validator_test_exclusions1_fail.csv";
+    private static final String TEST_EXCLUSIONS3_PASS_FILE_PATH = "/validator_test_exclusions2_pass.csv";
+    
+   
 
     private static final Logger logger = LoggerFactory.getLogger(DataValidatorTest.class);
 
@@ -182,4 +193,79 @@ public class DataValidatorTest
         // ===== verification =====
         // no result because an exception is expected
     }
+    
+    @Test
+    public void test_isValidGeneration_pass() throws FileNotFoundException, IOException
+    {
+        
+     // ===== set up =====
+        File dataFile = new File(
+                getClass().getResource(TEST_DATA3_PASS_FILE_PATH).getFile());
+       
+        File exclusionsFile = new File(
+                getClass().getResource(TEST_EXCLUSIONS3_PASS_FILE_PATH).getFile());
+        
+        DataReader dataReader = new DataReader();
+        List<SecretSanta> secretSantaList = dataReader.parseDataFileWithExclusionFile(
+                dataFile.getPath(), exclusionsFile.getPath());
+        
+        //creates an attendee to result map with no exclusions mapped to
+        //an attendee
+        Map<String, String> attendeeToResultMap = new HashMap<>();
+        attendeeToResultMap.put("TESTNAME1","TESTNAME5");
+        attendeeToResultMap.put("TESTNAME5","TESTNAME1");
+        attendeeToResultMap.put("TESTNAME4","TESTNAME3");
+        attendeeToResultMap.put("TESTNAME3","TESTNAME4");
+        attendeeToResultMap.put("TESTNAME2","TESTNAME6");
+        attendeeToResultMap.put("TESTNAME6","TESTNAME2");
+        
+      
+        // ===== test call =====
+        boolean result = this.validator.isValidGeneration(secretSantaList, attendeeToResultMap);
+       
+     // ===== verification =====
+        assertTrue(result);
+       
+
+
+    }
+    
+    @Test
+    public void test_isValidGeneration_fail() throws FileNotFoundException, IOException
+    {
+        
+     // ===== set up =====
+        File dataFile = new File(
+                getClass().getResource(TEST_DATA3_PASS_FILE_PATH).getFile());
+       
+        File exclusionsFile = new File(
+                getClass().getResource(TEST_EXCLUSIONS3_PASS_FILE_PATH).getFile());
+        
+        DataReader dataReader = new DataReader();
+        List<SecretSanta> secretSantaList = dataReader.parseDataFileWithExclusionFile(
+                dataFile.getPath(), exclusionsFile.getPath());
+        
+        //creates a attendee/result map specifically with exclusions assigned to
+        //an attendee
+        Map<String, String> attendeeToResultMap = new HashMap<>();
+        attendeeToResultMap.put("TESTNAME1","TESTNAME5");
+        attendeeToResultMap.put("TESTNAME5","TESTNAME1");
+        attendeeToResultMap.put("TESTNAME4","TESTNAME3");
+        attendeeToResultMap.put("TESTNAME3","TESTNAME2");
+        attendeeToResultMap.put("TESTNAME2","TESTNAME6");
+        attendeeToResultMap.put("TESTNAME6","TESTNAME4");
+        
+      
+        // ===== test call =====
+        boolean result = this.validator.isValidGeneration(secretSantaList, attendeeToResultMap);
+       
+     // ===== verification =====
+        assertFalse(result);
+       
+
+
+    }
+   
+       
+
 }
