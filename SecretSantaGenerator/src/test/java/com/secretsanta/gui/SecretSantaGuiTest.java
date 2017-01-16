f850c0e8d04dd6210aab17cef2288a1be031f5a0
Evan Tran <evant179@gmail.com>
2017-01-15 23:25:08 -0800

    Add Monocle to pom to support headless mode

--- SecretSantaGenerator/src/test/java/com/secretsanta/gui/SecretSantaGuiTest.java
+++ SecretSantaGenerator/src/test/java/com/secretsanta/gui/SecretSantaGuiTest.java
@@ -19,6 +19,14 @@ public class SecretSantaGuiTest extends ApplicationTest
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
+        if (Boolean.getBoolean("headless")) {
+            System.setProperty("testfx.robot", "glass");
+            System.setProperty("testfx.headless", "true");
+            System.setProperty("prism.order", "sw");
+            System.setProperty("prism.text", "t2k");
+            System.setProperty("java.awt.headless", "true");
+        }
+        
         ApplicationTest.launch(SecretSantaGui.class);
     }
 
